5d6512e5787ea9c4f35a12a960209250590282ec
Liz-Vazquez <54119354+Liz-Vazquez@users.noreply.github.com>
2020-12-06 14:50:21 -0600

    Creación escena nivel 5

---
+++ src/scenes/Scene_nivel5.js
@@ -0,0 +1,20 @@
+class Scene_nivel5 extends Phaser.Scene{
+    constructor(){
+        super({
+            key: 'Scene_nivel5'
+        });
+    }
+    init() {
+        console.log('Escena del nivel 5');
+    }
+    preload(){
+
+    }
+    create(){
+
+    }
+    update(time, delta) {
+
+    }
+}
+export default Scene_nivel5;
