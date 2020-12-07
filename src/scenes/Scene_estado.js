e6d6b1b9a312152c8deea6a5834f8772f3a3c1f4
Lluvia Naomy Carmona Avendaño <lluvia_naomy@hotmail.com>
2020-12-07 07:45:20 -0600

    Menú de boosters

--- src/scenes/Scene_estado.js
+++ src/scenes/Scene_estado.js
@@ -10,9 +10,10 @@ class Scene_estado extends Phaser.Scene{
     }
     preload(){
         this.load.path = './assets/estado/';      
-        this.load.image(['barraVida','cantDracmas', 'montoVida']);
+        this.load.image(['barraVida','cantDracmas', 'montoVida', 'boosters', 'botonBoosters', 'llave', 'escudo', 'videncia']);
     } 
     create() {
+        const eventos = Phaser.Input.Events;
         //Cantidad de dracmas
         this.data.set('score', 0);
 
@@ -34,6 +35,38 @@ class Scene_estado extends Phaser.Scene{
             key: 'montoVida',
             repeat: 3,
             setXY: { x:68, y: 57, stepX: 35 }
+        });
+
+        this.contenedor = this.add.container(29, 611);
+        //this.contenedor = this.add.container(815, 521);
+        //var container = this.add.container(400, 300);
+
+        this.botonBoosters = this.add.image(84, 18.5, 'botonBoosters').setInteractive();
+        this.boosters = this.add.image(83.5, 72.5, 'boosters');
+        this.llave = this.add.image(152.5, 74.5, 'llave');
+        this.escudo = this.add.image(14, 74.5, 'escudo');
+        this.videncia = this.add.image(85, 74.5, 'videncia');
+        this.llavesText = this.add.text(132, 77, this.data.list.llaves, { fontSize: '20px', fill: '#fff' });
+
+        this.contenedor.add(this.botonBoosters);
+        this.contenedor.add(this.boosters);
+        this.contenedor.add(this.llave);
+        this.contenedor.add(this.escudo);
+        this.contenedor.add(this.videncia);
+        this.contenedor.add(this.llavesText);
+
+        this.input.on(eventos.GAMEOBJECT_UP, (pointer, gameObject) => {
+            //this.contenedor = this.add.container(815, 521);
+            this.tweens = this.add.tween({
+                targets: [this.contenedor],
+                ease: 'Linear',
+                y: 521,
+                duration: 1000,
+                yoyo: true,
+                //repeatDelay:5000,
+                hold: 2000,
+                //delay: 3000
+            });
         });
         
         // this.grupod.children.iterate( (girar) => {
@@ -61,8 +94,50 @@ class Scene_estado extends Phaser.Scene{
             // this.score+=valorMoneda;
             // this.scoreText.setText(this.score);
             //console.log('Se ha emitido el evento score = ', this.score);
+
+        this.registry.events.on('adquiereLlave', () => {
+            //console.log("Recibe moneda");
+            this.data.list.llaves++;
+            this.llavesText.setText(this.data.list.llaves);
+            //console.log('Se ha emitido el evento score = ', this.score);
+        });
         });        
     }
+
+    click(){
+        this.contenedor = this.add.container(815, 521);
+        this.tweens = this.add.tween({
+            targets: [this.contenedor],
+            ease: 'Linear',
+            y: 521,
+            duration: 3000,
+            yoyo: true,
+            loopDelay: 2000
+            // onStart: () => {
+            //     nexus.body.enable = false;
+            //     this.golpe.play(this.musicConf2);
+                
+            //     //nexus.anims.play('nexus_dead');
+            // },
+            // onComplete: () => {
+            //     //nexus.body.enable = true;
+            //     nexus.x = 40;
+            //     nexus.y = 531;
+            //     nexus.setVelocity(0);
+            //     nexus.alpha = 1;
+            //     nexus.setFrame(0);
+            //     this.input.keyboard.enabled = true;
+
+            //     if(this.data.list.vidas!=0){
+            //         this.nexus.body.enable = true;
+            //     }
+            //     else{
+            //         this.scene.stop();
+            //     }
+            // }, 
+        });
+    }
+
     update(time, delta) {
 
     }
