2152c16a641030095acaaf4ff51bf1fd2391327a
Liz-Vazquez <54119354+Liz-Vazquez@users.noreply.github.com>
2020-12-07 06:52:05 -0600

    Aún no funciona el método para ganar

--- src/scenes/Scene_nivel5.js
+++ src/scenes/Scene_nivel5.js
@@ -9,8 +9,8 @@ class Scene_nivel5 extends Phaser.Scene{
     }
     preload(){
         this.load.path = './assets/nivel5/';
-        this.load.image(['plat1', 'plat2', 'plat3', 'plat4', 'plat5', 'plat6', 'plat7', 'plat8', 'plat9', 'plat11', 'salida', 'piso3']);
-        this.load.spritesheet('fondo', 'fondo1_6.png', {
+        this.load.image(['plat1', 'plat2', 'plat3', 'plat4', 'salida', 'piso3']);
+        this.load.spritesheet('fondo', 'fondo1_3.png', {
             frameWidth: 1000,
             frameHeight: 640,
         });
@@ -24,8 +24,6 @@ class Scene_nivel5 extends Phaser.Scene{
         this.load.animation('shadowAnim', '../shadow/shadow_all_anim.json');
         this.load.atlas('dracmas','../dracma/dracmas.png','../dracma/dracma_atlas.json');
         this.load.animation('dracmasAnim','../dracma/dracma_anim.json');
-        this.load.atlas('explosion','../explosion/explosion.png','../explosion/explosion_atlas.json');
-        this.load.animation('explosionAnim','../explosion/explosion_anim.json');
         this.load.atlas('mono_all', '../mono/mono_all.png','../mono/mono_all_atlas.json');
         this.load.animation('monoAnim', '../mono/mono_all_anim.json');
 
@@ -53,10 +51,10 @@ class Scene_nivel5 extends Phaser.Scene{
             key: 'fondo_anim',
             frames: this.anims.generateFrameNumbers('fondo', {
             start: 0,
-            end: 15
+            end: 23
             }),
             repeat: -1,
-            frameRate: 15 
+            frameRate: 23
         });
         this.fondo.anims.play('fondo_anim');
         this.fondo.body.setSize(1000, 10);
@@ -64,14 +62,14 @@ class Scene_nivel5 extends Phaser.Scene{
         this.scene.launch('Scene_estado');
         this.plataformas = this.physics.add.group();
         this.plataformas.create(45, 550, 'plat1').setName('plat1');
-        this.plataformas.create(250, 550, 'plat2').setName('plat2');
-        this.plataformas.create(45, 155, 'plat3').setName('plat3');
-        this.plataformas.create(650, 550, 'plat11').setName('plat11');
-        this.plataformas.create(930, 250, 'plat5').setName('plat5');
-        this.plataformas.create(500, 405, 'plat6').setName('plat6');
-        this.plataformas.create(350, 260, 'plat7').setName('plat7');
-        this.plataformas.create(960, 550, 'plat8').setName('plat8');
-        this.plataformas.create(660, 150, 'plat9').setName('plat9');
+        this.plataformas.create(250, 550, 'plat1').setName('plat2');
+        this.plataformas.create(45, 155, 'plat1').setName('plat3');
+        this.plataformas.create(650, 550, 'plat2').setName('plat4');
+        this.plataformas.create(930, 250, 'plat3').setName('plat5');
+        this.plataformas.create(500, 405, 'plat4').setName('plat6');
+        this.plataformas.create(350, 260, 'plat4').setName('plat7');
+        this.plataformas.create(960, 550, 'plat1').setName('plat8');
+        this.plataformas.create(660, 150, 'plat3').setName('plat9');
         this.plataformas.children.iterate( (p) => {
             p.setImmovable(true);
             p.body.allowGravity = false;   
@@ -80,7 +78,7 @@ class Scene_nivel5 extends Phaser.Scene{
         this.salida = this.add.image(930, 170, 'salida', 0).setInteractive().setScale(1.5);
         this.physics.add.existing(this.salida, true);
         this.salida.body.setSize(10, 110);
-        this.salida.body.setOffset(45, 0);
+        this.salida.body.setOffset(70, 0);
         this.nexus = this.physics.add.sprite(30,370, 'nexus_all', 0).setInteractive();
         this.nexus.setScale(1.2);
         this.nexus.setName('Nexus');
@@ -99,7 +97,6 @@ class Scene_nivel5 extends Phaser.Scene{
 
         this.dracmasIndv.children.iterate( (p) => {
             p.setScale(1.5);
-            //p.setDepth(3);
             p.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
             p.anims.play('dracma');
         });
@@ -111,7 +108,6 @@ class Scene_nivel5 extends Phaser.Scene{
         });
         this.dracmasGrupo1.children.iterate( (p) => {
             p.setScale(1.5);
-            //p.setDepth(3);
             p.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
             p.anims.play('dracma');
         });
@@ -124,7 +120,6 @@ class Scene_nivel5 extends Phaser.Scene{
 
         this.dracmasGrupo2.children.iterate( (p) => {
             p.setScale(1.5);
-            //p.setDepth(3);
             p.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
             p.anims.play('dracma');
         });
@@ -137,7 +132,6 @@ class Scene_nivel5 extends Phaser.Scene{
 
         this.dracmasGrupo3.children.iterate( (p) => {
             p.setScale(1.5);
-            //p.setDepth(3);
             p.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
             p.anims.play('dracma');
             p.body.allowGravity = false; 
@@ -165,16 +159,6 @@ class Scene_nivel5 extends Phaser.Scene{
         this.mono.body.setSize(65,85);
         this.mono.body.setOffset(1,-10);
         this.mono.anims.play('mono_eat');
-
-        /*this.fuego = this.physics.add.sprite(140,700,'explosion').setInteractive();
-        this.fuego.body.setCircle(35);
-        this.fuego.body.setImmovable = true;
-        this.fuego.body.setOffset(27,50);
-        this.fuego.anims.play('explotar');
-        this.fuego_2 = this.physics.add.sprite(815,700,'explosion').setInteractive();
-        this.fuego_2.body.setCircle(35);
-        this.fuego_2.body.setOffset(27,50);
-        this.fuego_2.anims.play('explotar');*/
 
         this.ball_1 = this.physics.add.sprite(140, 700, 'ball').setInteractive();
         this.ball_1.body.setCircle(35);
@@ -226,7 +210,7 @@ class Scene_nivel5 extends Phaser.Scene{
         this.physics.add.collider(this.dracmasIndv,this.plataformas.getChildren().find(v => v.name == "plat6"));
         this.physics.add.collider(this.dracmasIndv.getChildren().find(v => v.name == "dracmaIndv3"),this.plataformas.getChildren().find(v => v.name == "plat7"));
         this.physics.add.collider(this.dracmasGrupo1,this.plataformas);
-        this.physics.add.collider(this.dracmasGrupo2,this.plataformas.getChildren().find(v => v.name == "plat11"));
+        this.physics.add.collider(this.dracmasGrupo2,this.plataformas.getChildren().find(v => v.name == "plat4"));
         this.physics.add.collider(this.mono,this.plataformas);
 
         this.physics.add.overlap(this.nexus, this.dracmasIndv, this.recoger, null, this);
@@ -234,25 +218,10 @@ class Scene_nivel5 extends Phaser.Scene{
         this.physics.add.overlap(this.nexus, this.dracmasGrupo2, this.recoger, null, this);
         this.physics.add.overlap(this.nexus, this.dracmasGrupo3, this.recoger, null, this);
         this.physics.add.overlap(this.nexus, this.shadows, this.ataque, null, this);
-        /*this.physics.add.overlap(this.nexus, this.fuego, this.muere_nexus, null, this);
-        this.physics.add.overlap(this.nexus, this.fuego_2, this.muere_nexus, null, this);*/
         this.physics.add.overlap(this.nexus, this.ball_1, this.muere_nexus, null, this);
         this.physics.add.overlap(this.nexus, this.ball_2, this.muere_nexus, null, this);
         this.physics.add.overlap(this.nexus, this.mono, this.ataque_mono, null, this);
         this.physics.add.collider(this.nexus, this.salida, this.ganar, null, this);
-
-       /* this.tweenFuego = this.add.tween({
-            targets: [this.fuego, this.fuego_2],
-            ease: 'Power2',
-            //y:posInY+50,
-            y:{
-                value: -100,
-                duration: 2000
-            },
-            repeat: -1,
-            yoyo: true,
-            delay: 1000
-        });*/
 
         this.tweenBall = this.add.tween({
             targets: [this.ball_1, this.ball_2],
@@ -524,7 +493,7 @@ class Scene_nivel5 extends Phaser.Scene{
             target: 'Scene_puzzle1',
             duration: 1000,
             moveAbove: true,
-            onUpdate: this.transitionOut,
+            //onUpdate: this.transitionOut,
             data: { x: 500, y: 320 }
         });     
     }
